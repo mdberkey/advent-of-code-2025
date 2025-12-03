f18abaeffc84616c1d80e15872ca61ea184f6e14
mdberkey <mberkey2@gmail.com>
2025-12-03 00:52:25 -0500

    cleaup p2

--- 3/p2.py
+++ 3/p2.py
@@ -4,27 +4,25 @@ with open("input") as f:
 
     for line in f:
         line = line.strip()
-        line = [int(num) for num in line]
+        nums = [int(num) for num in line]
 
-        def get_max_and_ind(i, j): #inc, ex
-            curr_max = line[i]
+        def get_max_and_ind(i, j):
+            curr_max = nums[i]
             curr_ind = i
 
             for k in range(i, j):
-                if line[k] > curr_max:
-                    curr_max = line[k]
+                if nums[k] > curr_max:
+                    curr_max = nums[k]
                     curr_ind = k
             
             return curr_max, curr_ind + 1
 
-        joltage = []
-        curr_ind = 0
+        joltage = curr_ind = 0
         for i in range(11, -1, -1):
             curr_val, curr_ind = get_max_and_ind(curr_ind, len(line) - i)
-            joltage.append(str(curr_val))
+            joltage *= 10
+            joltage += curr_val
 
-        val = int("".join(joltage))
-        print(val)
-        res += val
+        res += joltage
 
     print(res)
