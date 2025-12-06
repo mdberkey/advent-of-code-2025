896eb797f6f9bbc63985d62a5b6541a3c069994a
Michael Berkey <mberkey2@gmail.com>
2025-12-06 11:33:55 -0500

    cleanup day 6 p1

--- 6/p1.py
+++ 6/p1.py
@@ -1,15 +1,11 @@
 
 with open("input") as f:
-    res = 0
-
     lines = [line.strip() for line in f]
-    
-    nums, ops = lines[:-1], lines[-1]
-    ops = ops.split()
-    
+    nums, ops = lines[:-1], lines[-1].split()
     vals = [1 if op == "*" else 0 for op in ops]
-    for num_list in nums:
-        for i, num in enumerate(num_list.split()):
+
+    for num_str in nums:
+        for i, num in enumerate(num_str.split()):
             if ops[i] == "*":
                 vals[i] *= int(num)
             else:
