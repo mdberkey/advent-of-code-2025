b5f8cf12f456d8955938e07c615d3d3017639c51
Michael Berkey <mberkey2@gmail.com>
2025-12-07 12:55:21 -0500

    add day 7 solutions

--- 7/p1.py
+++ 7/p1.py
@@ -2,17 +2,13 @@
 with open("input") as f:
     res = 0
     lines = [line.strip() for line in f]
-    start = lines[0].index("S")
-    beams = set()
-    beams.add(start)
+    beams = { lines[0].index("S") }
 
     for row in lines[1:]:
         for j in range(len(row)):
             if row[j] == "^" and j in beams:
-                if j > 0:
-                    beams.add(j - 1)
-                if j < len(row):
-                    beams.add(j + 1)
+                beams.add(j - 1)
+                beams.add(j + 1)
                 beams.remove(j)
                 res += 1
 
