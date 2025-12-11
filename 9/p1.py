c644c1f9f5ba9918de45e1553d04165cd6ba492d
mdberkey <mberkey2@gmail.com>
2025-12-10 23:55:10 -0500

    cleanup day 9 p1

--- 9/p1.py
+++ 9/p1.py
@@ -1,18 +1,12 @@
-import math
-from collections import deque, Counter
 
 with open("input") as f:
     lines = [line.strip() for line in f]
     res = 0
 
     for i, line in enumerate(lines):
-        c, r = line.split(",")
-        r = int(r)
-        c = int(c)
+        c, r = map(int, line.split(","))
         for j, line2 in enumerate(lines[i+1:]):
-            c2, r2 = line2.split(",")
-            r2 = int(r2)
-            c2 = int(c2)
+            c2, r2 = map(int, line2.split(","))
 
             res = max(res, abs(r - r2 + 1) * abs(c - c2 + 1))
 
